db7dc022b74a628c1c01af1aa39a9d0a56348e7a
shaiyonatan <shaiyonatan@gmail.com>
2020-04-19 17:44:06 +0300

    update config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -11,5 +11,5 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     password: process.env.DB_PASSWORD || dbConfig.password,
     database: process.env.DB_NAME || dbConfig.database,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true
+    synchronize: false
 }
