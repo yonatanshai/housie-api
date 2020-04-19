9528ad874277cb33b349ea472623fb49f4c52082
shaiyonatan <shaiyonatan@gmail.com>
2020-04-19 17:40:58 +0300

    update config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,11 +5,11 @@ const dbConfig = config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: dbConfig.type,
-    host: process.env.DATABASE_URL || dbConfig.host,
+    host: process.env.DB_HOST || dbConfig.host,
     port: process.env.DB_PORT || dbConfig.port,
     username: process.env.DB_USERNAME || dbConfig.username,
     password: process.env.DB_PASSWORD || dbConfig.password,
     database: process.env.DB_NAME || dbConfig.database,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: false
+    synchronize: true
 }
